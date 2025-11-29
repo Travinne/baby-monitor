92c14e1b7d9a2ffa1272ac0bc90a180338806f16
travinne <travinnetgm1@gmail.com>
2025-11-29 12:20:10 +0300

    baby 13

--- baby_backend/routes/register_routes.py
+++ baby_backend/routes/register_routes.py
@@ -1,24 +1,43 @@
 from flask import Blueprint, request, jsonify
-from werkzeug.security import check_password_hash
+from werkzeug.security import generate_password_hash
 from baby_backend.models import User
 from baby_backend.database import db
 import jwt
 import datetime
 
-login_bp = Blueprint('login_bp', __name__)
+register_bp = Blueprint('register_bp', __name__)
 
-@login_bp.route('', methods=['POST'])
-def login_user():
+@register_bp.route('', methods=['POST'])
+def register_user():
     data = request.get_json()
+
     username = data.get('username')
+    email = data.get('email')
     password = data.get('password')
+    confirm_password = data.get('confirm_password')
 
-    if not username or not password:
-        return jsonify({'message': 'Username and password are required'}), 400
+    if not username or not email or not password or not confirm_password:
+        return jsonify({'message': 'All fields are required'}), 400
 
-    user = User.query.filter_by(username=username).first()
-    if not user or not check_password_hash(user.password, password):
-        return jsonify({'message': 'Invalid username or password'}), 401
+    if password != confirm_password:
+        return jsonify({'message': 'Passwords do not match'}), 400
+
+    if User.query.filter_by(username=username).first():
+        return jsonify({'message': 'Username already exists'}), 409
+
+    if User.query.filter_by(email=email).first():
+        return jsonify({'message': 'Email already exists'}), 409
+
+    hashed_password = generate_password_hash(password)
+
+    user = User(
+        username=username,
+        email=email,
+        password=hashed_password
+    )
+
+    db.session.add(user)
+    db.session.commit()
 
     token = jwt.encode({
         'user_id': user.id,
@@ -26,7 +45,11 @@ def login_user():
     }, 'secret_key', algorithm='HS256')
 
     return jsonify({
-        'message': 'Login successful',
+        'message': 'Registration successful',
         'token': token,
-        'user': {'id': user.id, 'username': user.username, 'email': user.email}
-    }), 200
+        'user': {
+            'id': user.id,
+            'username': user.username,
+            'email': user.email
+        }
+    }), 201
