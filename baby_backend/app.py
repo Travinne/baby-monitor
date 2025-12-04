00961d30747199379e15fa90fd8eb979938e4a92
travinne <travinnetgm1@gmail.com>
2025-12-04 23:11:13 +0300

    [Fix] update database start up on app start up

--- baby_backend/app.py
+++ baby_backend/app.py
@@ -1,12 +1,11 @@
-from flask import Flask, send_from_directory, jsonify
+from flask import Flask, send_from_directory, jsonify, request
 from flask_cors import CORS
-from flask_jwt_extended import JWTManager
+from flask_jwt_extended import JWTManager, verify_jwt_in_request
 from flask_migrate import Migrate
 from werkzeug.utils import secure_filename
 import os
 
 from .database import db
-
 
 from .routes.allergies_routes import allergies_bp
 from .routes.bath_routes import bath_bp
@@ -19,11 +18,10 @@ from .routes.babyprofile_routes import babyprofile_bp
 from .routes.settings_routes import settings_bp
 from .routes.notification_routes import notification_bp
 from .routes.dashboard_routes import dashboard_bp
-from .routes.auth_routes import auth_bp  
+from .routes.auth_routes import auth_bp
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
-
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///baby_monitor.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -34,21 +32,37 @@ app.config["JWT_HEADER_NAME"] = "Authorization"
 app.config["JWT_HEADER_TYPE"] = "Bearer"
 app.config['UPLOAD_FOLDER'] = 'static/uploads/baby_photos'
 app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg'}
+
 os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
 jwt = JWTManager(app)
 db.init_app(app)
 migrate = Migrate(app, db)
 
+with app.app_context():
+        from baby_backend import models
+        db.create_all()
+
 app.register_blueprint(auth_bp, url_prefix="/api")
 
+PUBLIC_ROUTES = [
+    "/api/login",
+    "/api/register"
+]
+
+@app.before_request
+def protect_api_routes():
+    if request.path in PUBLIC_ROUTES:
+        return
+    if request.path.startswith("/api"):
+        try:
+            verify_jwt_in_request()
+        except:
+            return jsonify({"message": "Login Required"}), 401
 
 @app.route('/static/uploads/baby_photos/<path:filename>')
 def get_uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
-
-
-
 
 protected_blueprints = [
     (allergies_bp, "/api/allergies"),
@@ -65,16 +79,12 @@ protected_blueprints = [
 ]
 
 for bp, prefix in protected_blueprints:
-    app.register_blueprint(bp, url_prefix="/api")
+    app.register_blueprint(bp, url_prefix=prefix)
 
 @app.route('/')
 def home():
-    return jsonify({"message": " API Running, Login Required First!"})
-
+    return jsonify({"message": "API Running"})
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    import os
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port, debug=False, use_reloader=False)
