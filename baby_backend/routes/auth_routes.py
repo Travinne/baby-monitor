65edeadfcc3b712ef22e210a913ed81e1ab2e8dc
travinne <travinnetgm1@gmail.com>
2025-12-22 03:00:17 +0300

    Add Flask-Mail dependency

--- baby_backend/routes/auth_routes.py
+++ baby_backend/routes/auth_routes.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, request, jsonify, current_app
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity, decode_token
-from flask_mail import Message
 from ..models import User
 from baby_backend.database import db
 import datetime
